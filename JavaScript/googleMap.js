ed1a87b43f883a07f6b75cee3e9270b9c2c86d39
Kim, Dong-Hyeon <dhkim@hichart.net>
2015-12-16 14:12:45 +0900

    나의 팀 페이지 합체 / 연락처(나의팀)페이지 시작

---
+++ JavaScript/googleMap.js
@@ -0,0 +1,57 @@
+var map;
+var infowindow;
+
+function initMap() {
+  var pyrmont = {lat: -33.867, lng: 151.195};
+  var myHome = {lat: 37.473556, lng: 126.889478};
+
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: myHome,
+    zoom: 17
+  });
+
+  infowindow = new google.maps.InfoWindow();
+
+  /*
+  var service = new google.maps.places.PlacesService(map);
+  service.nearbySearch({
+    location: myHome,
+    radius: 500,
+    types: ['store']
+  }, callback);
+  */
+  createMakerMyHome();
+}
+
+function createMakerMyHome() {
+	var marker = new google.maps.Marker({
+		map: map,
+		position: {lat: 37.473556, lng: 126.889478}
+	});
+
+	google.maps.event.addListener(marker, 'click', function() {
+		infowindow.setContent('우리집');
+		infowindow.open(map, this);
+	});
+}
+
+function callback(results, status) {
+  if (status === google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      createMarker(results[i]);
+    }
+  }
+}
+
+function createMarker(place) {
+  var placeLoc = place.geometry.location;
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location
+  });
+
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.setContent(place.name);
+    infowindow.open(map, this);
+  });
+}
